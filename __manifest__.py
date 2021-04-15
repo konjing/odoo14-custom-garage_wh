10fd8ca9dbd92440e291bd039fe6ac2d64e8a0fe
konjing <konjingtuajing@gmail.com>
2021-04-15 12:29:23 +0700

    Test commit

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Garage Warehouse',
+    'version': '14.0.0.1.0',
+    'summary': 'Warehouse for simple garage',
+    'sequence': -10,
+    'description': """ """,
+    'category': 'Inventory/Inventory',
+    'website': '',
+    'images': [],
+    'depends': [],
+    'data': [],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
